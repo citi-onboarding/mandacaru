380df7779782e8dafddcdc666c3e4e320280a41f
FactorMouk <jvoa@cin.ufpe.br>
2017-12-08 01:18:20 -0300

    Sessão Equipe Adicionada

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(function(){
+
+	$(window).scroll(function (){
+		var currentScrollTop = $(window).scrollTop();
+
+	$('#black-overlay').css('opacity',currentScrollTop/$('#black-overlay').height());
+		});
+})(jQuery);
+  
