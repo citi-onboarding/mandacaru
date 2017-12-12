deb1c8f5b55d62c66e161b277e81c0916ca48194
gabrielaleal <gabriela.leal@hotmail.com>
2017-12-11 23:59:00 -0300

    contato pronto, scroll pronto, botões (frontend) prontos

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
 $(function(){
+	$(document).ready(function($){
+		$('.scroll').on('click', function(event){
+			event.preventDefault();
+			$('html,body').animate({scrollTop:$(this.hash).offset().top -105}, 500);
+		});
+	});
 	$('#menumobile').on('click', function (){
 		$('.menu').show();
 	});
